a72c8717db83838ee63ce18706f24411a66231a9
Guenole76 <54894682+Guenole76@users.noreply.github.com>
2021-05-18 14:24:03 +0200

    style

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -33,16 +33,16 @@ export default class Game extends Phaser.Scene
 
     create()
     {
-        this.labelScore = this.add.text(20, 20, "Score: 0", {font: "30px Arial", fill: "#ffff"} );
-        this.labelMarket = this.add.text(900, 580, "Market: 0", {font: "30px Arial", fill: "#ffff"} );
-        this.labelDev= this.add.text(900, 260, "Dev: 0", {font: "30px Arial", fill: "#ffff"} );
-        this.labelDesign = this.add.text(900, 850, "Design: 0", {font: "30px Arial", fill: "#ffff"} );
-        this.labelPointPerClick = this.add.text(0, 850, "Point par Clique: 1", {font: "30px Arial", fill: "#ffff"} );
-        this.labelTemps = this.add.text(0, 800, "Points toutes les : ", {font: "30px Arial", fill: "#ffff"} );
-        this.labelPointsTemps = this.add.text(0, 750, "nombre de point(s) par tic  : ", {font: "30px Arial", fill: "#ffff"} );
-        this.coutmarket = this.add.text(900, 300, "Cout Market: 10", {font: "30px Arial", fill: "#ffff"} );
-        this.coutdev = this.add.text(900, 0, "Cout Dev: 10", {font: "30px Arial", fill: "#ffff"} );
-        this.coutdesign = this.add.text(900, 620, "Cout Design: 10", {font: "30px Arial", fill: "#ffff"} );
+        this.labelScore = this.add.text(20, 20, "Score: 0", {font: "70px Times New Roman", fill: "#FFE200"} );
+        this.labelMarket = this.add.text(850, 450, "Market: 0", {font: "30px Times New Roman", fill: "#FF91A9"} );
+        this.labelDev= this.add.text(850, 190, "Dev: 0", {font: "30px Times New Roman", fill: "#FFE200"} );
+        this.labelDesign = this.add.text(850, 760, "Design: 0", {font: "30px Times New Roman", fill: "#00CFFF"} );
+        this.labelPointPerClick = this.add.text(0, 800, "Point par Clique: 1", {font: "30px Times New Roman", fill: "#FF91A9"} );
+        this.labelTemps = this.add.text(0, 750, "Points toutes les : ", {font: "30px Times New Roman", fill: "#FFE200"} );
+        this.labelPointsTemps = this.add.text(0, 850, "nombre de point(s) par tic  : ", {font: "30px Times New Roman", fill: "#00CFFF"} );
+        this.coutmarket = this.add.text(850, 370, "Cout Market: 10", {font: "30px Times New Roman", fill: "#FF91A9"} );
+        this.coutdev = this.add.text(850, 100, "Cout Dev: 10", {font: "30px Times New Roman", fill: "#FFE200"} );
+        this.coutdesign = this.add.text(850, 680, "Cout Design: 10", {font: "30px Times New Roman", fill: "#00CFFF"} );
         var nws = this.add.image(300, 400, "nws").setScale(0.2);
 
         nws.setInteractive();
@@ -55,7 +55,7 @@ export default class Game extends Phaser.Scene
             this
         );
 
-        var dev = this.add.image(1000, 150, "dev").setScale(1);
+        var dev = this.add.image(750, 150, "dev").setScale(1);
 
         dev.setInteractive();
 
@@ -71,7 +71,7 @@ export default class Game extends Phaser.Scene
             this
         );
 
-        var market = this.add.image(1000, 450, "market").setScale(1);
+        var market = this.add.image(750, 450, "market").setScale(1);
 
         market.setInteractive();
 
@@ -88,7 +88,7 @@ export default class Game extends Phaser.Scene
             this
         );
 
-        var design = this.add.image(1000, 750, "design").setScale(1);
+        var design = this.add.image(750, 750, "design").setScale(1);
 
         design.setInteractive();
 
